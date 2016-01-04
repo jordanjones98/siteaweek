d52adabd0d09412dbff94089a2991038b65ccd9b
Jordan Jones <jordanalanjones@gmail.com>
2016-01-03 20:28:20 -0600

    added a lot of design

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-  $("button").click(function(){
+  $(".joinnow").click(function(){
     $('html,body').animate({
         scrollTop: $(".join").offset().top},
         'slow');
   });
 });
+$('#container').imagesLoaded()
+  .always( function( instance ) {
+    console.log('all images loaded');
+  })
+  .done( function( instance ) {
+    console.log('all images successfully loaded');
+  })
+  .fail( function() {
+    console.log('all images loaded, at least one is broken');
+  })
+  .progress( function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+  });
